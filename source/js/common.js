b1bdad602306034e40f111fe3de33a7318f5799c
admin <soinproduction@gmail.com>
2021-09-07 06:23:56 +0300

    fix some problems

--- source/js/common.js
+++ source/js/common.js
@@ -404,6 +404,19 @@ for (b = 0; b < acc.length; b++) {
       panel.classList.add('active');
     }
   });
+};
+
+
+function show_hide_password(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
 }
 
 
